053e13be4e8f27055756cccdba42e0a8412f62d7
Sergey Fomenko <artem.work749@gmail.com>
2023-03-08 18:33:00 +0300

    first commit

---
+++ src/main/java/com/example/testdeploy/GreetingController.java
@@ -0,0 +1,13 @@
+package com.example.testdeploy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class GreetingController {
+    @GetMapping("/greeting")
+    public String greeting() {
+        return "greeting";
+    }
+}
